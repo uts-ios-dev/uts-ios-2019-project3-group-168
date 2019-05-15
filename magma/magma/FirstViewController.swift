b0da1220046c0d6c574198b6d576ca850b8b0da7
Justin Wilkin <justin.wilkin@digizoo.com.au>
2019-05-15 15:14:56 +1000

    Initial Project Creation

---
+++ magma/magma/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  magma
+//
+//  Created by Justin Wilkin on 15/5/19.
+//  Copyright © 2019 Code Candy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ magma/magma/SecondViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondViewController.swift
+//  magma
+//
+//  Created by Justin Wilkin on 15/5/19.
+//  Copyright © 2019 Code Candy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
