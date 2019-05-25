d7caee1abf1d2bf3c90a46b2022eeaa0083f333b
Rohit Maharjan <oneworld.spirit3@gmail.com>
2019-05-26 01:57:25 +1000

    -Added contents in the settings page for having basic functionalities

--- magma/magma/Controllers/SettingsViewController.swift
+++ magma/magma/Controllers/SettingsViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class SettingsViewController: UIViewController {
     
+    @IBOutlet weak var enableTouchIdView: UIView!
+    @IBOutlet weak var changeDetailView: UIView!
+    @IBOutlet weak var logOutView: UIView!
+    
     // MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        enableTouchIdView.layer.borderWidth = 1
+        enableTouchIdView.layer.borderColor = UIColor.white.cgColor
+        
+        changeDetailView.layer.borderWidth = 1
+        changeDetailView.layer.borderColor = UIColor.white.cgColor
+        
+        logOutView.layer.borderWidth = 1
+        logOutView.layer.borderColor = UIColor.white.cgColor
     }
     
 }
